98899e74fdf4edefcbd77193d99322e7c626d150
siky <yyymalicious@gmail.com>
2025-03-28 16:35:18 +0800

    修改文件布局

--- js/collections.js
+++ js/collections.js
@@ -5,7 +5,7 @@ const productData = {
             id: 'NA001',
             name: 'Modern Aviator',
             price: 89.00,
-            images: ['img/1-1.jpg', 'img/1-2.jpg', 'img/1-3.jpg'],
+            images: ['/img/1-1.jpg', '/img/1-2.jpg'],
             colors: ['#000000', '#3d85c6', '#b45f06'],
             category: 'Aviator'
         },
@@ -13,7 +13,7 @@ const productData = {
             id: 'NA002',
             name: 'Modern Aviator',
             price: 89.00,
-            images: ['img/1-1.jpg', 'img/1-2.jpg', 'img/1-3.jpg'],
+            images: ['/img/1-1.jpg', '/img/1-2.jpg'],
             colors: ['#000000', '#3d85c6', '#b45f06'],
             category: 'Aviator'
         },
@@ -21,7 +21,7 @@ const productData = {
             id: 'NA003',
             name: 'Modern Aviator',
             price: 89.00,
-            images: ['img/1-1.jpg', 'img/1-2.jpg', 'img/1-3.jpg'],
+            images: ['/img/1-1.jpg', '/img/1-2.jpg'],
             colors: ['#000000', '#3d85c6', '#b45f06'],
             category: 'Aviator'
         },
@@ -29,7 +29,7 @@ const productData = {
             id: 'NA004',
             name: 'Modern Aviator',
             price: 89.00,
-            images: ['img/1-1.jpg', 'img/1-2.jpg', 'img/1-3.jpg'],
+            images: ['/img/1-1.jpg', '/img/1-2.jpg'],
             colors: ['#000000', '#3d85c6', '#b45f06'],
             category: 'Aviator'
         },
@@ -37,7 +37,7 @@ const productData = {
             id: 'NA005',
             name: 'Modern Aviator',
             price: 89.00,
-            images: ['img/1-1.jpg', 'img/1-2.jpg', 'img/1-3.jpg'],
+            images: ['/img/1-1.jpg', '/img/1-2.jpg'],
             colors: ['#000000', '#3d85c6', '#b45f06'],
             category: 'Aviator'
         },
@@ -45,7 +45,7 @@ const productData = {
             id: 'NA006',
             name: 'Modern Aviator',
             price: 89.00,
-            images: ['img/1-1.jpg', 'img/1-2.jpg', 'img/1-3.jpg'],
+            images: ['/img/1-1.jpg', '/img/1-2.jpg'],
             colors: ['#000000', '#3d85c6', '#b45f06'],
             category: 'Aviator'
         }
@@ -55,7 +55,7 @@ const productData = {
             id: 'CL001',
             name: 'Classic Wayfarer',
             price: 79.00,
-            images: ['img/2-1.jpg', 'img/2-2.jpg', 'img/2-3.jpg'],
+            images: ['/img/2-1.jpg', '/img/2-2.jpg'],
             colors: ['#000000', '#6d351a'],
             category: 'Wayfarer'
         },
@@ -63,7 +63,7 @@ const productData = {
             id: 'CL002',
             name: 'Classic Wayfarer',
             price: 79.00,
-            images: ['img/2-1.jpg', 'img/2-2.jpg'],
+            images: ['/img/2-1.jpg', '/img/2-2.jpg'],
             colors: ['#000000', '#6d351a'],
             category: 'Wayfarer'
         },
@@ -71,7 +71,7 @@ const productData = {
             id: 'CL003',
             name: 'Classic Wayfarer',
             price: 79.00,
-            images: ['img/2-1.jpg', 'img/2-2.jpg'],
+            images: ['/img/2-1.jpg', '/img/2-2.jpg'],
             colors: ['#000000', '#6d351a'],
             category: 'Wayfarer'
         },
@@ -79,7 +79,7 @@ const productData = {
             id: 'CL004',
             name: 'Classic Wayfarer',
             price: 79.00,
-            images: ['img/2-1.jpg', 'img/2-2.jpg'],
+            images: ['/img/2-1.jpg', '/img/2-2.jpg'],
             colors: ['#000000', '#6d351a'],
             category: 'Wayfarer'
         }   
@@ -89,7 +89,7 @@ const productData = {
             id: 'SS001',
             name: 'Summer Breeze',
             price: 99.00,
-            images: ['img/3-1.jpg', 'img/3-2.jpg', 'img/3-3.jpg'],
+            images: ['/img/3-1.jpg', '/img/3-2.jpg'],
             colors: ['#f6b26b', '#000000'],
             category: 'Round'
         },
@@ -100,7 +100,7 @@ const productData = {
             id: 'FW001',
             name: 'Winter Classic',
             price: 89.00,
-            images: ['img/4-1.jpg', 'img/4-2.jpg', 'img/4-3.jpg'],
+            images: ['/img/4-1.jpg', '/img/4-2.jpg'],
             colors: ['#000000', '#6aa84f'],
             category: 'Oversized'
         },
@@ -165,7 +165,7 @@ function createProductCard(product) {
                 <div>
                     <div class="product-category">${product.category}</div>
                     <h3 class="product-title"><a href="#">${product.name}</a></h3>
-                    <div class="product-price">€${product.price.toFixed(2)}</div>
+                    <div class="product-price">$${product.price.toFixed(2)}</div>
                 </div>
                 <div class="product-colors">
                     ${product.colors.map(color => `
