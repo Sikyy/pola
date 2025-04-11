127c7b7cc3b0eef9ddc0bc9a267b9163fb5faf43
siky <yyymalicious@gmail.com>
2025-04-11 13:37:58 +0800

    Fix

--- src/js/collections.js
+++ src/js/collections.js
@@ -3,94 +3,166 @@ const productData = {
     newArrivals: [
         {
             id: 'NA001',
-            name: 'Modern Aviator',
-            price: 89.00,
+            name: 'MS20003',
+            price: 59.99,
             images: ['img/1-1.jpg', 'img/1-2.jpg'],
             colors: ['#000000', '#3d85c6', '#b45f06'],
-            category: 'Aviator'
+            category: 'newArrivals'
         },
         {
             id: 'NA002',
-            name: 'Modern Aviator',
-            price: 89.00,
-            images: ['img/1-1.jpg', 'img/1-2.jpg'],
+            name: 'OSM200630',
+            price: 59.99,
+            images: ['img/1-3.jpg', 'img/1-4.jpg'],
             colors: ['#000000', '#3d85c6', '#b45f06'],
-            category: 'Aviator'
+            category: 'newArrivals'
         },
         {
             id: 'NA003',
-            name: 'Modern Aviator',
-            price: 89.00,
-            images: ['img/1-1.jpg', 'img/1-2.jpg'],
+            name: 'OSM200648',
+            price: 59.99,
+            images: ['img/1-5.jpg', 'img/1-6.jpg'],
             colors: ['#000000', '#3d85c6', '#b45f06'],
-            category: 'Aviator'
+            category: 'newArrivals'
         },
         {
             id: 'NA004',
-            name: 'Modern Aviator',
-            price: 89.00,
-            images: ['img/1-1.jpg', 'img/1-2.jpg'],
+            name: 'OSM200703',
+            price: 79.99,
+            images: ['img/1-7.jpg', 'img/1-8.jpg'],
             colors: ['#000000', '#3d85c6', '#b45f06'],
-            category: 'Aviator'
+            category: 'newArrivals'
         },
         {
             id: 'NA005',
-            name: 'Modern Aviator',
-            price: 89.00,
-            images: ['img/1-1.jpg', 'img/1-2.jpg'],
+            name: 'OSM200706',
+            price: 59.99,
+            images: ['img/1-9.jpg', 'img/1-10.jpg'],
             colors: ['#000000', '#3d85c6', '#b45f06'],
-            category: 'Aviator'
+            category: 'newArrivals'
         },
         {
             id: 'NA006',
-            name: 'Modern Aviator',
-            price: 89.00,
-            images: ['img/1-1.jpg', 'img/1-2.jpg'],
+            name: 'OSM200709',
+            price: 59.99,
+            images: ['img/1-11.jpg', 'img/1-12.jpg'],
             colors: ['#000000', '#3d85c6', '#b45f06'],
-            category: 'Aviator'
+            category: 'newArrivals'
         },
         {
-            id: 'NA006',
-            name: 'Modern Aviator',
-            price: 89.00,
-            images: ['img/1-3.jpg', 'img/1-4.jpg'],
+            id: 'NA007',
+            name: 'OSM200710',
+            price: 59.99,
+            images: ['img/1-13.jpg', 'img/1-14.jpg'],
             colors: ['#000000', '#3d85c6', '#b45f06'],
-            category: 'Aviator'
+            category: 'newArrivals'
+        },
+        {
+            id: 'NA008',
+            name: 'R16M163J1',
+            price: 59.99,
+            images: ['img/1-15.jpg', 'img/1-16.jpg'],
+            colors: ['#000000', '#3d85c6', '#b45f06'],
+            category: 'newArrivals'
         },
     ],
     classic: [
         {
             id: 'CL001',
-            name: 'Classic Wayfarer',
-            price: 79.00,
+            name: 'OSA200470',
+            price: 79.99,
             images: ['img/2-1.jpg', 'img/2-2.jpg'],
             colors: ['#000000', '#6d351a'],
-            category: 'Wayfarer'
+            category: 'classic'
         },
         { 
             id: 'CL002',
-            name: 'Classic Wayfarer',
-            price: 79.00,
-            images: ['img/2-1.jpg', 'img/2-2.jpg'],
+            name: 'OSA200472',
+            price: 79.99,
+            images: ['img/2-3.jpg', 'img/2-4.jpg'],
             colors: ['#000000', '#6d351a'],
-            category: 'Wayfarer'
+            category: 'classic'
         },
         {   
             id: 'CL003',
-            name: 'Classic Wayfarer',
-            price: 79.00,
-            images: ['img/2-1.jpg', 'img/2-2.jpg'],
+            name: 'OSM200663',
+            price: 59.99,
+            images: ['img/2-5.jpg', 'img/2-6.jpg'],
             colors: ['#000000', '#6d351a'],
-            category: 'Wayfarer'
+            category: 'classic'
         },
         {
             id: 'CL004',
-            name: 'Classic Wayfarer',
-            price: 79.00,
-            images: ['img/2-1.jpg', 'img/2-2.jpg'],
+            name: 'OSM200704',
+            price: 59.99,
+            images: ['img/2-7.jpg', 'img/2-8.jpg'],
             colors: ['#000000', '#6d351a'],
-            category: 'Wayfarer'
-        }   
+            category: 'classic'
+        }, 
+        {
+            id: 'CL005',
+            name: 'OSM200705',
+            price: 59.99,
+            images: ['img/2-9.jpg', 'img/2-10.jpg'],
+            colors: ['#000000', '#6d351a'],
+            category: 'classic'
+        },
+        {
+            id: 'CL006',
+            name: 'OSM200708',
+            price: 59.99,
+            images: ['img/2-11.jpg', 'img/2-12.jpg'],
+            colors: ['#000000', '#6d351a'],
+            category: 'classic'
+        },
+        {
+            id: 'CL007',
+            name: 'OSM200713',
+            price: 59.99,
+            images: ['img/2-13.jpg', 'img/2-14.jpg'],
+            colors: ['#000000', '#6d351a'],
+            category: 'classic'
+        },
+        {
+            id: 'CL008',
+            name: 'osm200715',
+            price: 79.99,
+            images: ['img/2-15.jpg', 'img/2-16.jpg'],
+            colors: ['#000000', '#6d351a'],
+            category: 'classic'
+        },
+        {
+            id: 'CL009',
+            name: 'OSM208041A1',
+            price: 59.99,
+            images: ['img/2-17.jpg', 'img/2-18.jpg'],
+            colors: ['#000000', '#6d351a'],
+            category: 'classic'
+        },
+        {
+            id: 'CL010',
+            name: 'OSM208120',
+            price: 59.99,
+            images: ['img/2-19.jpg', 'img/2-20.jpg'],
+            colors: ['#000000', '#6d351a'],
+            category: 'classic'
+        },
+        {
+            id: 'CL011',
+            name: 'OSM208120',
+            price: 59.99,
+            images: ['img/2-21.jpg', 'img/2-22.jpg'],
+            colors: ['#000000', '#6d351a'],
+            category: 'classic'
+        },
+        {
+            id: 'CL012',
+            name: 'OSM208211A1',
+            price: 59.99,
+            images: ['img/2-23.jpg', 'img/2-24.jpg'],
+            colors: ['#000000', '#6d351a'],
+            category: 'classic'
+        },  
     ],
     springSummer: [
         {
@@ -156,18 +228,105 @@ const productData = {
             images: ['img/3-15.jpg', 'img/3-16.jpg'],
             colors: ['#f6b26b', '#000000'],
             category: 'springSummer'
+        },
+        {
+            id: 'SS009',
+            name: 'R16M163J1',
+            price: 59.99,
+            images: ['img/3-17.jpg', 'img/3-18.jpg'],
+            colors: ['#f6b26b', '#000000'],
+            category: 'springSummer'
+        },
+        {
+            id: 'SS010',
+            name: 'MS20003',
+            price: 59.99,
+            images: ['img/3-19.jpg', 'img/3-20.jpg'],
+            colors: ['#f6b26b', '#000000'],
+            category: 'springSummer'
         },// Add more products...
     ],
     fallWinter: [
         {
             id: 'FW001',
-            name: 'Winter Classic',
-            price: 89.00,
+            name: 'OSA200470',
+            price: 79.99,
             images: ['img/4-1.jpg', 'img/4-2.jpg'],
             colors: ['#000000', '#6aa84f'],
-            category: 'Oversized'
+            category: 'fallWinter'
         },
-        // Add more products...
+        {
+            id: 'FW002',
+            name: 'OSM200663',
+            price: 59.99,
+            images: ['img/4-3.jpg', 'img/4-4.jpg'],
+            colors: ['#000000', '#6aa84f'],
+            category: 'fallWinter'
+        },
+        {
+            id: 'FW003',
+            name: 'OSM200703',
+            price: 79.99,
+            images: ['img/4-5.jpg', 'img/4-6.jpg'],
+            colors: ['#000000', '#6aa84f'],
+            category: 'fallWinter'
+        },
+        {
+            id: 'FW004',
+            name: 'OSM200704',
+            price: 59.99,
+            images: ['img/4-7.jpg', 'img/4-8.jpg'],
+            colors: ['#000000', '#6aa84f'],
+            category: 'fallWinter'
+        },
+        {
+            id: 'FW005',
+            name: 'OSM200705',
+            price: 59.99,
+            images: ['img/4-9.jpg', 'img/4-10.jpg'],
+            colors: ['#000000', '#6aa84f'],
+            category: 'fallWinter'
+        },
+        {
+            id: 'FW006',
+            name: 'OSM200706',
+            price: 59.99,
+            images: ['img/4-11.jpg', 'img/4-12.jpg'],
+            colors: ['#000000', '#6aa84f'],
+            category: 'fallWinter'
+        },
+        {
+            id: 'FW007',
+            name: 'OSM200715',
+            price: 59.99,
+            images: ['img/4-13.jpg', 'img/4-14.jpg'],
+            colors: ['#000000', '#6aa84f'],
+            category: 'fallWinter'
+        },
+        {
+            id: 'FW008',
+            name: 'OSM208041A1',
+            price: 59.99,
+            images: ['img/4-15.jpg', 'img/4-16.jpg'],
+            colors: ['#000000', '#6aa84f'],
+            category: 'fallWinter'
+        },
+        {
+            id: 'FW009',
+            name: 'OSM208121',
+            price: 59.99,
+            images: ['img/4-17.jpg', 'img/4-18.jpg'],
+            colors: ['#000000', '#6aa84f'],
+            category: 'fallWinter'
+        },
+        {
+            id: 'FW010',
+            name: 'OSM208211A1',
+            price: 59.99,
+            images: ['img/4-19.jpg', 'img/4-20.jpg'],
+            colors: ['#000000', '#6aa84f'],
+            category: 'fallWinter'
+        },// Add more products...
     ]
 };
 
